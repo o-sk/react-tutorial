92eb3e08b9d60ee33b3493286ba216dcb3622c3e
o-sk <m.ohsuka@gmail.com>
2019-10-13 23:52:07 +0900

    Highlight winner line

--- src/index.js
+++ src/index.js
@@ -3,8 +3,12 @@ import ReactDOM from 'react-dom';
 import './index.css';
 
 function Square(props) {
+  let className="square";
+  if (props.highlight) {
+    className += " highlight";
+  }
   return (
-    <button className="square" onClick={props.onClick}>
+    <button className={className} onClick={props.onClick}>
       {props.value}
     </button>
   );
@@ -32,7 +36,7 @@ function calculateWinner(squares) {
   for (let i = 0; i < lines.length; i++) {
     const [a, b, c] = lines[i];
     if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
-      return squares[a];
+      return [squares[a], a, b, c];
     }
   }
   return null;
@@ -51,11 +55,16 @@ class Board extends React.Component {
     });
   }
 
-  renderSquare(i) {
+  renderSquare(i, winner) {
+    let highlight = false;
+    if (winner && (winner[1] === i || winner[2] === i || winner[3] === i)) {
+      highlight = true;
+    }
     return (
       <Square
         key={i}
         value={this.props.squares[i]}
+        highlight={highlight}
         onClick={() => this.props.onClick(i)}
       />
     );
@@ -68,7 +77,7 @@ class Board extends React.Component {
       <div>
         {rows.map((row) =>
           <div className="board-row" key={row}>
-            {cols.map((col) => this.renderSquare(row * 3 + col))}
+            {cols.map((col) => this.renderSquare(row * 3 + col, this.props.winner))}
           </div>
         )}
       </div>
@@ -145,7 +154,7 @@ class Game extends React.Component {
 
     let status;
     if (winner) {
-      status = 'Winner: ' + winner;
+      status = 'Winner: ' + winner[0];
     } else if (this.state.stepNumber === 9) {
       status = 'Draw';
     } else {
@@ -157,6 +166,7 @@ class Game extends React.Component {
         <div className="game-board">
           <Board
             squares={current.squares}
+            winner={winner}
             onClick={(i) => this.handleClick(i)}
           />
         </div>
